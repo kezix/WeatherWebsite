911c871364faa5018086ee22bb7684ac9bbde8db
KeziX <88567983+kezix@users.noreply.github.com>
2023-02-01 21:01:04 +0100

    😎

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
